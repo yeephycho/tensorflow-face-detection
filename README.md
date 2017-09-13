9d21d3961c699ba43908171d75c62e9770435089
yeephycho <yeephycho@github.com>
2017-09-13 11:08:40 +0800

    modified:   .gitignore 	modified:   README.md 	new file:   inference_video_face.py 	new file:   model/frozen_inference_graph_face.pb 	new file:   protos/__init__.py 	new file:   protos/face_label_map.pbtxt 	new file:   protos/string_int_label_map_pb2.py 	new file:   utils/__init__.py 	new file:   utils/label_map_util.py 	new file:   utils/visualization_utils_color.py

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# tensorflow_face_detection
-A mobilenet SSD based pretrained face detector, powered by tensorflow object detection api, trained by WIDERFACE dataset.
+# Tensorflow Face Detector
+A mobilenet SSD(single shot multibox detector) based face detector with pretrained model provided, powered by tensorflow [object detection api](https://github.com/tensorflow/models/tree/master/object_detection), trained by [WIDERFACE dataset](http://mmlab.ie.cuhk.edu.hk/projects/WIDERFace/).
+
+# Features
+Fast speed, run 60fps on a nvidia GTX1080 GPU.
+
+Less memory, requires less than 400Mb GPU memory for inference.
+
+Robust to different poses, this feature is credit to [WIDERFACE dataset](http://mmlab.ie.cuhk.edu.hk/projects/WIDERFace/), I manually cleaned the dataset to balance the precision and recall trade off.
+
+Parallel video processing, can inference multiple input simultaneously, I tested to process 4 videos on a single GPU card at the same time, the speed is still competitive, and there's still room to accommodate more processes.
+
+![Parallel data processing](https://github.com/yeephycho/tensorflow-face-detection/blob/master/res/test_result.png?raw=true "Show result")
+
+# Dependencies
+Tensorflow 1.3
+
+Tensorflow object detection api (Please follow the official installation instruction, otherwise, I cannot guarantee that you can run the code)
+
+OpenCV python
+
+# Usage
+Click [Youtube](https://youtu.be/gw4CVz7SPEs) to view the effect or [Youku, currently unavaliable]().
+
+Click [here](https://drive.google.com/open?id=0B5ttP5kO_loUdWZWZVVrN2VmWFk) to download the pre-trained model from google drive.
+
+Put the model under the model folder.
+
+Put your test video (mp4 format) under the media folder, rename it as test.mp4. 
+
+At the source root
+```python
+python inference_video_face.py
+```
+
+After finished the processing, find the output video at media folder.
+
+# LICENSE
+Usage of the model is under the license of Apache 2.0.
+
+The code is based on GOOGLE tensorflow object detection api. Please refer to the license of tensorflow.
+
+Dataset is based on WIDERFACE dataset. Please refer to the license to the WIDERFACE license.
---
+++ inference_video_face.py
@@ -0,0 +1,110 @@
+import numpy as np
+import os
+import six.moves.urllib as urllib
+import sys
+import tarfile
+import tensorflow as tf
+import zipfile
+import time
+import cv2
+
+from collections import defaultdict
+from io import StringIO
+from matplotlib import pyplot as plt
+from PIL import Image
+
+
+sys.path.append("..")
+
+from utils import label_map_util
+from utils import visualization_utils_color as vis_util
+
+
+# Path to frozen detection graph. This is the actual model that is used for the object detection.
+PATH_TO_CKPT = './model/frozen_inference_graph_face.pb'
+
+# List of the strings that is used to add correct label for each box.
+PATH_TO_LABELS = './protos/face_label_map.pbtxt'
+
+NUM_CLASSES = 2
+
+label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
+
+def load_image_into_numpy_array(image):
+  (im_width, im_height) = image.size
+  return np.array(image.getdata()).reshape(
+      (im_height, im_width, 3)).astype(np.uint8)
+
+# Size, in inches, of the output images.
+# IMAGE_SIZE = (12, 8)
+import matplotlib.image as mpimg
+
+
+cap = cv2.VideoCapture("./media/test.mp4")
+out = cv2.VideoWriter("./media/test_out.avi", 0, 25.0, (1920, 1080))
+
+
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+    od_graph_def = tf.GraphDef()
+    with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+        serialized_graph = fid.read()
+        od_graph_def.ParseFromString(serialized_graph)
+        tf.import_graph_def(od_graph_def, name='')
+
+with detection_graph.as_default():
+  config = tf.ConfigProto()
+  config.gpu_options.allow_growth=True
+  with tf.Session(graph=detection_graph, config=config) as sess:
+    frame_num = 1490;
+    while(frame_num):
+      frame_num -= 1
+      ret, image = cap.read()
+      if(ret ==0):
+          break
+      image_np = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+      # the array based representation of the image will be used later in order to prepare the
+      # result image with boxes and labels on it.
+      # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+      image_np_expanded = np.expand_dims(image_np, axis=0)
+      image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+      # Each box represents a part of the image where a particular object was detected.
+      boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+      # Each score represent how level of confidence for each of the objects.
+      # Score is shown on the result image, together with the class label.
+      scores = detection_graph.get_tensor_by_name('detection_scores:0')
+      classes = detection_graph.get_tensor_by_name('detection_classes:0')
+      num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+      # Actual detection.
+      start_time = time.time()
+      (boxes, scores, classes, num_detections) = sess.run(
+          [boxes, scores, classes, num_detections],
+          feed_dict={image_tensor: image_np_expanded})
+      elapsed_time = time.time() - start_time
+      print('inference time cost: {}'.format(elapsed_time))
+      #print(boxes.shape, boxes)
+      #print(scores.shape,scores)
+      #print(classes.shape,classes)
+      #print(num_detections)
+      # Visualization of the results of a detection.
+      vis_util.visualize_boxes_and_labels_on_image_array(
+#          image_np,
+          image,
+          np.squeeze(boxes),
+          np.squeeze(classes).astype(np.int32),
+          np.squeeze(scores),
+          category_index,
+          use_normalized_coordinates=True,
+          line_thickness=4)
+      out.write(image)
+#      plt.figure(figsize=IMAGE_SIZE)
+#      plt.imshow(image)
+#      plt.imsave(image_path + "_output.png", image_np)
+#      plt.imsave("_output.png", image_np)
+
+
+    cap.release()
+    out.release()
